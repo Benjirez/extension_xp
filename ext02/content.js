1c35446b56768bd6046d3ed7f7af44f64668e1b8
benjirez <benjirez@gmail.com>
2018-04-16 17:14:32 -0700

    adds draggable div to body

---
+++ ext02/content.js
@@ -0,0 +1,6 @@
+// content.js
+//$(document).ready(function(){
+	//alert('yo');
+	$('body').append("<div id = 'drag1' style='background: black'><h1>Appended text</h1></div>");
+	$( "#drag1" ).draggable();
+//});
