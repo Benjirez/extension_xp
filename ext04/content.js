394f1b4e06ac993ddd0dcfcffa294efb97fa5e2e
benjirez <benjirez@gmail.com>
2018-04-17 17:11:56 -0700

    ext04 $().get to /ext loads into cp

---
+++ ext04/content.js
@@ -0,0 +1,21 @@
+// content.js
+
+
+$('body').append( cp );
+$( "#drag1" ).draggable();
+
+var speak1Text = '';
+
+$(document).keypress(function(event) {
+     // $("#speak1").html('Handler for .keypress() called. - <h2>' + event.charCode +'</h2>');
+	 speak1Text += String.fromCharCode( event.charCode );
+	  $("#speak1").html( '<h2>' + speak1Text + '</h2>');
+});
+
+$.get("http://127.0.0.1:3000/ext", function(data, status){
+	$('#speak2').html('<div>' + data + '</div>');
+		$(".toggler").click(function(){
+			$(this).children(".toggles").toggle();
+    });
+});
+
---
+++ ext04/cp.js
@@ -0,0 +1,12 @@
+// cp
+
+var cp = `<div id = 'drag1' >
+	<div style='display: block; background: darkgrey; width: 500px; height: 60px;'>
+	<button id='b1' style='width: 100%; ' > B-1 </button><br>
+	<div id='speak1'></div>
+	</div>
+	<div id='speak2' style='display: block; background: grey; width: 500px; height: 400px; overflow: scroll;'>sup fo</div>
+
+</div>`;
+
+//display: flex;  justify-content: center; //align-items: center;
